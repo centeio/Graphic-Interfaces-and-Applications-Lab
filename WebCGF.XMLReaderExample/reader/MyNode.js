f1a73bad3d66202a32dc13e7d91bd17abd280f43
centeio <c.centeiojorge@outlook.pt>
2016-12-27 19:32:50 +0000

    My Node

--- WebCGF.XMLReaderExample/reader/MyNode.js
+++ WebCGF.XMLReaderExample/reader/MyNode.js
@@ -1,12 +1,32 @@
-function MyNode(scene, player, name){
+function MyNode(scene){
     this.scene = scene;
-    this.node = new MyCylinder(scene, 0.3, 0, 1, 20, 20);
-    this.player = player;
-    this.name = name;
+    this.node = new MyCylinder(scene, 0.1, 0.05, 0.6, 20, 20);
+    this.bottom = new MySphere(scene, 0.2, 20, 20);
+    this.base = new MySphere(scene, 0.3, 20, 20);
+    this.top = new MySphere(scene, 0.1, 20, 20);
 }
 
 MyNode.prototype.display = function(){
     this.scene.pushMatrix();
+ //   this.scene.translate(0,0.15,0);    
+    this.scene.scale(1,0.3,1);
+    this.base.display();        
+    this.scene.popMatrix();
+    
+    this.scene.pushMatrix();
+    this.scene.translate(0,0.9,0);    
+//    this.scene.scale(0.8,1,0.8);
+    this.top.display();        
+    this.scene.popMatrix();
+    
+    this.scene.pushMatrix();
+    this.scene.translate(0,0.2,0);    
+//    this.scene.scale(0.8,1,0.8);
+    this.bottom.display();        
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.translate(0,0.3,0);        
     this.scene.rotate(-Math.PI/2,1,0,0);
     this.node.display();
     this.scene.popMatrix();
